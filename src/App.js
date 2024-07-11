3c8edb67bb6eceb6e274a2719f60b30667d2cb9e
Dorian Kita <dorian.kita90@gmail.com>
2024-07-11 18:33:36 +0200

    created and used dynamic request to create new event and edit already existing and send data to backend via the same component using different method from actions

--- src/App.js
+++ src/App.js
@@ -28,11 +28,12 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/RootLayout";
 import EventLayout from "./pages/EventLayout";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from "./components/EventForm";
 
 const router = createBrowserRouter([
   {
@@ -60,11 +61,19 @@ const router = createBrowserRouter([
                 action: deleteEventAction,
                 element: <EventDetailPage />,
               },
-              { path: "edit", element: <EditEventPage /> },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
 
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
       },
     ],
